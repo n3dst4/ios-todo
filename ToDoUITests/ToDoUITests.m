c45666137b89c884ef1a717b0df5271c350bb1c5
git@lumphammer.com <n3dst4@gmail.com>
2016-08-14 18:49:21 +0100

    half a working ui test

---
+++ ToDoUITests/ToDoUITests.m
@@ -0,0 +1,50 @@
+//
+//  ToDoUITests.m
+//  ToDoUITests
+//
+//  Created by Neil de Carteret on 14/08/2016.
+//  Copyright © 2016 Neil de Carteret. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ToDoUITests : XCTestCase
+
+@end
+
+@implementation ToDoUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"Add"] tap];
+    XCUIElement *textField = app.textFields[@"Title"];
+    [textField typeText:@"hello"];
+    
+    
+//    [[[[[[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeTextField].element
+    [app.buttons[@"Save"] tap];
+    
+}
+
+@end
